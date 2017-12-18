c47098874b0492b251e9d40851a5cdb7d4e4c5ae
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-12-18 15:38:31 +0800

    add event and refs

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <p>
-      simple-form works!
-    </p>
+    <div>
+      <input #comment type="text"/>
+      <button (click)="onClick(comment)">Post</button> 
+    </div>
   `,
   styles: []
 })
@@ -14,6 +15,10 @@ export class SimpleFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(value) {
+    console.log(value.value)
   }
 
 }
